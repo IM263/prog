88a0de7c874bce279dbc8700e0661b9cfb4f8701
IM263 <125740246+IM263@users.noreply.github.com>
2025-05-22 14:08:01 +0300

    Update code.cpp

--- Leggage/code.cpp
+++ Leggage/code.cpp
@@ -2,7 +2,7 @@
 
 Luggage(int quantity, int mass) : quantity(quantity), mass(mass) {}
 
-void task_1(num_pass){
+void task_1(num_pass,count_mass,count_quantity){
     for (int i = 0; i<num_pass; i++){
         arr[i]
     }
